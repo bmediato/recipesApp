cec6be6587a1f9440ffe6ec85f410676fffe0e33
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-12-06 16:23:00 -0300

    fix: Lint

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,6 @@ import { saveRecipes } from '../redux/actions';
 
 class SearchBar extends Component {
   state = {
-    // recipes: [],
     searchInput: '',
     radioValue: '',
   };
@@ -56,14 +55,7 @@ class SearchBar extends Component {
 
   handleButtonClick = async () => {
     const { history, page, dispatch } = this.props;
-    // const NUMBER_MAX_ARRAY = 11;
     const recipes = await this.fetchRecipes();
-    // if (recipes.length > NUMBER_MAX_ARRAY) {
-    //   /* Aqui o splice serve para caso o array seja maior do que 12,
-    //   ele vai retirar os elementos do indice 12 até o ultimo */
-    //   recipes
-    //     .splice(NUMBER_MAX_ARRAY + 1, recipes.length - 1);
-    // }
     switch (recipes.length) {
     case 0:
       return global.alert('Sorry, we haven\'t found any recipes for these filters.');
@@ -83,79 +75,60 @@ class SearchBar extends Component {
   };
 
   render() {
-    const { searchInput, recipes } = this.state;
+    const { searchInput } = this.state;
     return (
-      <>
-        <form>
-          <input
-            data-testid="search-input"
-            placeholder="Search"
-            type="text"
-            name="searchInput"
-            value={ searchInput }
-            onChange={ this.onInputChange }
-          />
+      <form>
+        <input
+          data-testid="search-input"
+          placeholder="Search"
+          type="text"
+          name="searchInput"
+          value={ searchInput }
+          onChange={ this.onInputChange }
+        />
 
-          <label htmlFor="ingredient">
-            Ingredient
-            <input
-              type="radio"
-              data-testid="ingredient-search-radio"
-              id="ingredient"
-              name="radioInput"
-              value="ingredient"
-              onClick={ this.handleRadioButton }
-            />
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              type="radio"
-              data-testid="name-search-radio"
-              id="name"
-              name="radioInput"
-              value="name"
-              onClick={ this.handleRadioButton }
-            />
-          </label>
-          <label htmlFor="firstLetter">
-            First letter
-            <input
-              type="radio"
-              data-testid="first-letter-search-radio"
-              id="firstLetter"
-              name="radioInput"
-              value="firstLetter"
-              onClick={ this.handleRadioButton }
-            />
-          </label>
-          <button
-            data-testid="exec-search-btn"
-            type="button"
-            onClick={ this.handleButtonClick }
-            disabled={ !this.isButtonDisabled() }
-          >
-            Search
-          </button>
-        </form>
-        {/* {recipes.map((recipe, i) => (
-          <section
-            data-testid={ `${i}-recipe-card` }
-            key={ recipe.idMeal || recipe.idDrink }
-          >
-            <img
-              data-testid={ `${i}-card-img` }
-              src={ recipe.strMealThumb || recipe.strDrinkThumb }
-              alt="Imagem ilustrativa da receita"
-            />
-            <div
-              data-testid={ `${i}-card-name` }
-            >
-              { recipe.strMeal || recipe.strDrink }
-            </div>
-          </section> */}
-        ))}
-      </>
+        <label htmlFor="ingredient">
+          Ingredient
+          <input
+            type="radio"
+            data-testid="ingredient-search-radio"
+            id="ingredient"
+            name="radioInput"
+            value="ingredient"
+            onClick={ this.handleRadioButton }
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="radio"
+            data-testid="name-search-radio"
+            id="name"
+            name="radioInput"
+            value="name"
+            onClick={ this.handleRadioButton }
+          />
+        </label>
+        <label htmlFor="firstLetter">
+          First letter
+          <input
+            type="radio"
+            data-testid="first-letter-search-radio"
+            id="firstLetter"
+            name="radioInput"
+            value="firstLetter"
+            onClick={ this.handleRadioButton }
+          />
+        </label>
+        <button
+          data-testid="exec-search-btn"
+          type="button"
+          onClick={ this.handleButtonClick }
+          disabled={ !this.isButtonDisabled() }
+        >
+          Search
+        </button>
+      </form>
     );
   }
 }
@@ -168,6 +141,7 @@ SearchBar.propTypes = {
     push: PropTypes.func,
   }).isRequired,
   page: PropTypes.string.isRequired,
+  dispatch: PropTypes.shape({}).isRequired,
 };
 
 const mapStateToProps = (state) => ({
