3c2ca08a6552ca04acb06266276285861d09c798
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-12-06 17:28:16 -0300

    feat: Lógica para mudar o botão quando a receita estiver em andamento

--- src/components/ButtonStartRecipe.js
+++ src/components/ButtonStartRecipe.js
@@ -1,25 +1,53 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 class ButtonStartRecipe extends Component {
+  state = {
+    recipeIsDone: false,
+    recipeInProgress: false,
+  };
+
+  componentDidMount() {
+    this.isRecipeDone();
+    this.isRecipeInProgress();
+  }
+
   isRecipeDone = () => {
     const { id } = this.props;
     /* recipesLocalStorage vai tentar pegar as receitas do localStorage caso a chave não exista
     ele vai ficar como um array vazio, apenas para não quebrar o código */
-    const recipesLocalStorage = JSON.parse(localStorage.getItem('doneRecipes')) || [];
-    return recipesLocalStorage.some((recipe) => recipe.id === id);
+    const allRecipesDone = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    const recipeIsDone = allRecipesDone.some((recipe) => recipe.id === id);
+    return this.setState({ recipeIsDone });
+  };
+
+  isRecipeInProgress = () => {
+    const { id } = this.props;
+    const allRecipesInProgress = JSON.parse(localStorage
+      .getItem('inProgressRecipes')) || { meals: {}, drinks: {} };
+    const values = Object.values(allRecipesInProgress);
+
+    const verificaIds = () => values.some((elements) => {
+      const ids = Object.keys(elements);
+      return ids.includes(id);
+    });
+
+    if (verificaIds()) {
+      return this.setState({ recipeInProgress: true });
+    }
+    return this.setState({ recipeInProgress: false });
   };
 
   render() {
+    const { recipeInProgress, recipeIsDone } = this.state;
     return (
       <button
         data-testid="start-recipe-btn"
         type="button"
         className="button__start__recipe"
-        style={ { display: this.isRecipeDone() ? 'none' : 'flex' } }
+        style={ { display: recipeIsDone ? 'none' : 'flex' } }
       >
-        Start Recipe
+        {recipeInProgress ? 'Continue Recipe' : 'Start Recipe'}
       </button>
     );
   }
@@ -29,8 +57,4 @@ ButtonStartRecipe.propTypes = {
   id: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  id: state.searchRecipes.id,
-});
-
-export default connect(mapStateToProps)(ButtonStartRecipe);
+export default ButtonStartRecipe;
