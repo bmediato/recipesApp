113dbf0183b718ccee6bccff5e8c05938b8a2519
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-12-08 19:19:56 -0300

    fix: lint

--- src/components/FilterFavorite.js
+++ src/components/FilterFavorite.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
+// import React, { Component } from 'react';
+// // import PropTypes from 'prop-types';
+// import mealIcon from '../images/mealIcon.svg';
+// import drinkIcon from '../images/drinkIcon.svg';
 
-class FilterFavorite extends Component {
-  constructor() {
-    super();
-    this.state = {
-      favoriteRecipes: [],
-    };
-  }
+// class FilterFavorite extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       favoriteRecipes: [],
+//     };
+//   }
 
-  btnAll = () => {
-    // remove os filtros,retorna todas as receitas
-  };
+//   btnAll = () => {
+//     // remove os filtros,retorna todas as receitas
+//   };
 
-  btnMeals = () => {
-    // retorna todas as receitas de comida
-    favoriteRecipes.filter
-  };
+//   btnMeals = () => {
+//     // retorna todas as receitas de comida
+//     favoriteRecipes.filter
+//   };
 
-  btnDrinks = () => {
-    // retorna todas as receitas de bebidas
-  };
+//   btnDrinks = () => {
+//     // retorna todas as receitas de bebidas
+//   };
 
-  render() {
-    return (
+//   render() {
+//     return (
 
-      <div>
-        <button
-          type="button"
-          data-testid="filter-by-all-btn"
-          onClick={ this.btnAll }
-        >
-          All
-          <img
-            src={ drinkIcon }
-            alt="ShareIcon"
-          />
-        </button>
+//       <div>
+//         <button
+//           type="button"
+//           data-testid="filter-by-all-btn"
+//           onClick={ this.btnAll }
+//         >
+//           All
+//           <img
+//             src={ drinkIcon }
+//             alt="ShareIcon"
+//           />
+//         </button>
 
-        <button
-          type="button"
-          data-testid="filter-by-meal-btn"
-          onClick={ this.btnMeals }
-        >
-          Meals
-          <img
-            src={ mealIcon }
-            alt="ShareIcon"
-          />
-        </button>
+//         <button
+//           type="button"
+//           data-testid="filter-by-meal-btn"
+//           onClick={ this.btnMeals }
+//         >
+//           Meals
+//           <img
+//             src={ mealIcon }
+//             alt="ShareIcon"
+//           />
+//         </button>
 
-        <button
-          type="button"
-          data-testid="filter-by-drink-btn"
-          onClick={ this.btnDrinks }
-        >
-          Drinks
-          <img
-            src={ drinkIcon }
-            alt="ShareIcon"
-          />
-        </button>
-      </div>
-    );
-  }
-}
+//         <button
+//           type="button"
+//           data-testid="filter-by-drink-btn"
+//           onClick={ this.btnDrinks }
+//         >
+//           Drinks
+//           <img
+//             src={ drinkIcon }
+//             alt="ShareIcon"
+//           />
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-export default FilterFavorite;
+// export default FilterFavorite;
