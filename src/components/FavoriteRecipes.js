a7c7b48a8e4355652dc8d91cd1e80f2769647b5b
heidegomes <heidegc@hotmail.com>
2022-12-08 13:32:24 -0300

    feat: pagina de receitas favoritas em construção

--- src/components/FavoriteRecipes.js
+++ src/components/FavoriteRecipes.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Filter from './Filter';
-import Card from './Card';
 
 class FavoriteRecipes extends Component {
   constructor() {
@@ -21,6 +21,14 @@ class FavoriteRecipes extends Component {
     return favoriteRecipes;
   };
 
+  linkNames = (id) => {
+    const { page } = this.props;
+    if (page === 'meals') {
+      return `meals/${id}`;
+    }
+    return `drinks/${id}`;
+  };
+
   render() {
     const { history } = this.props;
     const { favoriteRecipes } = this.state;
@@ -37,19 +45,37 @@ class FavoriteRecipes extends Component {
         <ul>
           {
             favoriteRecipes.map((element, index) => (
-              <Card key={ index } />
+              <li key={ index }>
+                <Link to={ this.linkNames(element.idMeal || element.idDrink) }>
+                  <img
+                    src=""
+                    alt=""
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {`${element.nationality} - ${element.category}`}
+                </p>
+
+                <Link to={ this.linkNames(element.idMeal || element.idDrink) }>
+                  <p data-testid={ `${index}-horizontal-name` }>
+                    {element.name}
+                  </p>
+                </Link>
+
+                {/* <Share /> */}
+                {/* <Favorite /> */}
+              </li>
             ))
           }
-          {/* <Share /> */}
-          {/* <Favorite /> */}
-
         </ul>
-
       </div>
     );
   }
 }
 FavoriteRecipes.propTypes = {
   history: PropTypes.shape({}).isRequired,
+  page: PropTypes.string.isRequired,
 };
 export default FavoriteRecipes;
